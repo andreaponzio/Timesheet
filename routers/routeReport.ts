c3fff8cbd5a36077bcb08019179220c5e23453d2
Andrea Ponzio <andrea.ponzio@gmail.com>
2026-01-02 11:30:40 +0100

    Attività: campo ore non più obbligatorio; Report: 1) aggunta colonna ExtraInfo; 2) gestito errore Excel quando non riesce a generarlo; 3) sistemata la gestione della data;

--- routers/routeReport.ts
+++ routers/routeReport.ts
@@ -12,10 +12,12 @@ import IField = SqlGen.IField;
 import Sign = SqlGen.Sign;
 import Option = SqlGen.Option;
 import {out} from "../public/config.json";
+import CDatabase from "../core/CDatabase";
 
 interface IExcel {
    date: string;
    hour: number;
+   extrainfo: string;
    note: string;
    activity_description: string;
    activity_internal_ref: string;
@@ -62,22 +64,27 @@ let getWeekWorkday = (year: number, month: number, load: boolean = true): IWeekW
  * Dichiarazioni locali.
  */
 export let router: Router = express.Router();
+let excelDate: Date;
+let excelError: string = "";
 
 /**
  * Permette di analizzare i dati settimanali.
  */
 router.get("/week", (request: Request, response: Response) => {
-   let year: string;
-   let month: string;
+   let db: CDatabase;
 
-   year = new Date().getFullYear().toString();
-   month = (new Date().getMonth() + 1).toString();
+   db = new CDatabase();
+
+   // Imposta data di default:
+   if(!excelDate)
+      excelDate = new Date();
 
    response.render("app", {
       view: objectType.week_list,
       data: {
-         date: `${year}-${month}`,
-         display: false
+         date: db.convertDate(excelDate, 2),
+         display: false,
+         error: excelError,
       }
    });
 });
@@ -93,6 +100,7 @@ router.get("/week/:id", (request: Request, response: Response) => {
    let counter: number = 0;
    let workbook: any;
    let worksheet: any;
+   let col: number;
    let cell: any;
 
    o = new CWorkday();
@@ -121,19 +129,57 @@ router.get("/week/:id", (request: Request, response: Response) => {
          {header: "Data", key: "date", width: 20},
          {header: "Giorno", key: "day", width: 10},
          {header: "Ore", key: "hour", width: 8},
+         {header: "Extrainfo", key: "extrainfo", width: 10},
          {header: "Note", key: "note", width: 80},
          {header: "Descrizione", key: "activity_description", width: 80},
          {header: "Rif. interno", key: "activity_internal_ref", width: 80},
          {header: "WBS", key: "wbs_internal_ref", width: 25},
          {header: "Luogo", key: "place", width: 20},
       ];
-      cell = worksheet.getCell("A1")
-      cell.font = {bold: true};
-      cell.fill = {
-         type: "pattern",
-         pattern: "solid",
-         fgColor: {argb: "66CC00"}
-      };
+
+      // Assegna colore all'intestazione di colonna:
+      for(col = 1; col < 9; col++) {
+         switch(col) {
+            case 1:
+               cell = worksheet.getCell("A1");
+               break;
+
+            case 2:
+               cell = worksheet.getCell("B1");
+               break;
+
+            case 3:
+               cell = worksheet.getCell("C1");
+               break;
+
+            case 4:
+               cell = worksheet.getCell("D1");
+               break;
+
+            case 5:
+               cell = worksheet.getCell("E1");
+               break;
+
+            case 6:
+               cell = worksheet.getCell("F1");
+               break;
+
+            case 7:
+               cell = worksheet.getCell("G1");
+               break;
+
+            case 8:
+               cell = worksheet.getCell("H1");
+               break;
+         }
+         cell.font = {bold: true};
+         cell.fill = {
+            type: "pattern",
+            pattern: "solid",
+            fgColor: {argb: "66CC00"}
+         };
+         cell = undefined;
+      }
 
       // Valorizza righe:
       for(let r of data)
@@ -149,26 +195,34 @@ router.get("/week/:id", (request: Request, response: Response) => {
          });
    }
 
-   // Scrive file Excel e riporta nella pagina principale::
-   workbook.xlsx.writeFile(`${out}W${request.params.id}.xlsx`).then();
-   response.redirect("/report/week");
+   // Scrive file Excel e riporta nella pagina principale:
+   workbook.xlsx.writeFile(`${out}W${request.params.id}.xlsx`).then(() => {
+      excelError = "";
+      response.redirect("/report/week");
+   }).catch((err) => {
+      excelError = err.message;
+      response.redirect("/report/week");
+   });
 });
 
 /**
- * Visualizza i dati settimanali per il mese scelto. Ci sarà anche la possibilità di
- * scaricarli tutti o solo la singola settimana.
+ * Visualizza i dati settimanali per il mese scelto.
  */
 router.post("/week", (request: Request, response: Response) => {
+   let db: CDatabase;
    let date: string[];
+
+   db = new CDatabase();
 
    // Suddivide la data scelta in anno e mese e genera le settimane:
    date = request.body.month.split("-");
+   excelDate = new Date(parseInt(date[0]), parseInt(date[1]) - 1, 1);
 
    // Riporta sulla pagina:
    response.render("app", {
       view: objectType.week_list,
       data: {
-         date: `${parseInt(date[0])}-${parseInt(date[1])}`,
+         date: db.convertDate(excelDate, 2),
          workday: getWeekWorkday(parseInt(date[0]), parseInt(date[1]) - 1),
          display: true
       }
