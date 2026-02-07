c3d2a6bc5bd165e736cd5f2d3d4bf9f9e42faf93
Andrea ponzio <andrea.ponzio@gmail.com>
2026-02-07 13:20:51 +0100

    Nello stato delle TR aggiunta possibilità di filtro sull'attività

--- routers/routeRequest.ts
+++ routers/routeRequest.ts
@@ -9,6 +9,68 @@ import CActivity from "../core/CActivity";
 import CRequest, {IRequestSummarize} from "../core/CRequest";
 
 /**
+ * Funzioni locali.
+ */
+let listOfRequest = (request: Request, tr: CRequest): IRequestSummarize[] => {
+   let listOfRequest: IRequestSummarize[] = [];
+   let prodsys: string[];
+   let activity: number;
+   let type: number;
+   let filter: string = "";
+   let where: string = "";
+
+   // Recupera parametri di filtro:
+   if(request.body !== undefined) {
+      activity = parseInt(request.body.activity as string);
+      type = parseInt(request.body.ftype as string);
+   }
+   else {
+      activity = 0;
+      type = 0;
+   }
+
+   // Legge sistemi finali e prepara filtro:
+   try {
+      prodsys = tr.executeAll("SELECT * FROM main.prod_system;") as string[];
+      prodsys.forEach(s => {
+         filter = filter + `,'${s["systemid"]}'`;
+      });
+      filter = filter.slice(1);
+   }
+   catch(e) {
+   }
+
+   // Aggiunge al filtro anche la parte riguardante l'attività:
+   if(activity)
+      where = `WHERE activity_id = ${activity} `;
+
+   // Filtra le richieste in base al tipo filtro:
+   switch(type) {
+      case 0:
+         listOfRequest = tr.executeAll(`SELECT *
+                                        FROM main.request_status ${where}
+                                        GROUP BY id;`) as IRequestSummarize[];
+         break;
+
+      case 1:
+         filter = `HAVING SUM(COALESCE(systemid IN (${filter}), 0)) = 0`;
+         listOfRequest = tr.executeAll(`SELECT *
+                                        FROM main.request_status ${where}
+                                        GROUP BY id ${filter};`) as IRequestSummarize[];
+         break;
+
+      case 2:
+         filter = `HAVING SUM(COALESCE(systemid IN (${filter}), 0)) > 0`;
+         listOfRequest = tr.executeAll(`SELECT *
+                                        FROM main.request_status ${where}
+                                        GROUP BY id ${filter};`) as IRequestSummarize[];
+         break;
+   }
+
+   return listOfRequest;
+}
+
+/**
  * Dichiarazioni locali.
  */
 export let router: Router = express.Router();
@@ -38,17 +100,35 @@ router.get("/:id", (request: Request, response: Response) => {
    let activity: CActivity;
 
    tr = new CRequest();
-   activity = new CActivity();
-   tr.load(parseInt(request.params.id as string));
-   activity.load(tr.activity);
 
-   response.render("app", {
-      view: objectType.request_details,
-      data: {
-         request: tr.data,
-         activity: activity.data
-      }
-   });
+   // Gestisce sia la selezione di una TR sia quando viene selezionato il link allo stato
+   // delle TR.
+   // Stato delle TR:
+   if(isNaN(parseInt(request.params.id as string)))
+      response.render("app", {
+         view: objectType.tr_status,
+         data: {
+            activity_list: CTool.getActivity(new CActivity()),
+            activity: 0,
+            request: listOfRequest(request, tr),
+            checked: "0"
+         }
+      });
+
+   // Specifica TR:
+   else {
+      activity = new CActivity();
+      tr.load(parseInt(request.params.id as string));
+      activity.load(tr.activity);
+
+      response.render("app", {
+         view: objectType.request_details,
+         data: {
+            request: tr.data,
+            activity: activity.data
+         }
+      });
+   }
 });
 
 /**
@@ -58,11 +138,27 @@ router.post("/:id", (request: Request, response: Response) => {
    let tr: CRequest;
 
    tr = new CRequest();
+
+   // Gestisce il POST sia quando selezionata una specifica TR sia quando si è nella
+   // pagina degli stati delle TR:
    try {
-      tr.load(parseInt(request.params.id as string));
-      tr.note = request.body.note;
-      tr.saveNote();
-      response.redirect("/request/");
+      if(isNaN(parseInt(request.params.id as string))) {
+         response.render("app", {
+            view: objectType.tr_status,
+            data: {
+               activity_list: CTool.getActivity(new CActivity()),
+               activity: parseInt(request.body.activity as string),
+               request: listOfRequest(request, new CRequest()),
+               checked: request.body.ftype
+            }
+         });
+      }
+      else {
+         tr.load(parseInt(request.params.id as string));
+         tr.note = request.body.note;
+         tr.saveNote();
+         response.redirect("/request/");
+      }
    }
    catch(e) {
       response.render(`/request/${request.params.id}`, {
@@ -135,57 +231,4 @@ router.delete("/:id", (request: Request, response: Response) => {
    finally {
       tr = undefined;
    }
-});
-
-/**
- * Permette di analizzare lo stato delle TR.
- */
-router.get("/trstatus/:id", (request: Request, response: Response) => {
-   let tr: CRequest;
-   let listOfRequest: IRequestSummarize[];
-   let prodsys: string[];
-   let id: number[];
-   let filter: string = "";
-
-   tr = new CRequest();
-
-   // Legge sistemi finali e prepara filtro:
-   try {
-      prodsys = tr.executeAll("SELECT * FROM main.prod_system;") as string[];
-      prodsys.forEach(s => {
-         filter = filter + `,'${s["systemid"]}'`;
-      });
-      filter = filter.slice(1);
-   }
-   catch(e) {
-   }
-
-   // Filtra le richieste in base al tipo filtro:
-   switch(parseInt(request.params.id as string)) {
-      case 0:
-         listOfRequest = tr.executeAll("SELECT * FROM main.request_status GROUP BY id;") as IRequestSummarize[];
-         break;
-
-      case 1:
-         filter = `HAVING SUM(COALESCE(systemid IN (${filter}), 0)) = 0`;
-         listOfRequest = tr.executeAll(`SELECT *
-                                        FROM main.request_status
-                                        GROUP BY id ${filter};`) as IRequestSummarize[];
-         break;
-
-      case 2:
-         filter = `HAVING SUM(COALESCE(systemid IN (${filter}), 0)) > 0`;
-         listOfRequest = tr.executeAll(`SELECT *
-                                        FROM main.request_status
-                                        GROUP BY id ${filter};`) as IRequestSummarize[];
-         break;
-   }
-
-   response.render("app", {
-      view: objectType.tr_status,
-      data: {
-         request: listOfRequest,
-         checked: parseInt(request.params.id as string)
-      }
-   });
 });
