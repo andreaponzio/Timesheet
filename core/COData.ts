06ed9c64c0e634b22496aa49fbe7b0e12b04c9c1
Andrea Ponzio <andrea.ponzio@gmail.com>
2026-08-10 17:56:12 +0200

    Completamento OData Customer (get)

--- core/COData.ts
+++ core/COData.ts
@@ -4,93 +4,165 @@
  */
 import {Request} from "express";
 
-export interface IOData {
-   entity: string;
-   key: string;
-   expand: string;
-}
-
 export default class COData {
    /**
-    * Proprietà private.
+    * Scompone il dato IS_PAGING in arrivo dalla chiamata esterna.
+    * @public
+    * @static
+    * @param request express.Request
+    * @return istruzione LIMIT.
     */
-   private readonly _query: Map<string, string>;
-   private readonly _entity: string[];
-   private readonly _originalUrl: string;
+   public static limit(request: Request): string {
+      let result: string;
+      let top: number;
+      let skip: number;
 
-   /**
-    * Scompone l'URL originale per determinare la prima Entity con il suo
-    * eventuale filtro.
-    */
-   private mainEntity(): string[] {
-      let listToken: string[] = [];
-      let token: string = "";
-      let offset: number = 0;
-      let c: string;
+      // Prepara l'istruzione LIMIT:
+      top = parseInt(request.body["parameter"]["paging"]["top"]);
+      skip = parseInt(request.body["parameter"]["paging"]["skip"]);
+      if(top === 0)
+         top = -1;
+      if(skip === 0)
+         skip = -1;
 
-      while(this._originalUrl.charAt(offset) !== "?") {
-         c = this._originalUrl.charAt(offset);
-         switch(c) {
-            case ')':
-            case "(":
-               if(token) {
-                  listToken.push(token);
-                  token = "";
-               }
-               listToken.push(c);
-               break;
+      // Prepara l'istruzione LIMIT:
+      result = `LIMIT ${top} OFFSET ${skip}`;
 
-            case "/":
-               if(token) {
-                  listToken.push(token);
-                  token = "";
-               }
-               listToken.push(c);
-               break;
-
-            case "?":
-               if(token) {
-                  listToken.push(token);
-                  token = "";
-               }
-               break;
-
-            default:
-               token += c;
-         }
-         offset++;
-      }
-
-      // Aggiunge ultimo token:
-      if(token)
-         listToken.push(token);
-
-      // Restituisce la lista dei token:
-      return listToken;
+      // Risultato:
+      return result;
    }
 
    /**
-    * Costruttore.
+    * Compone l'istruzione WHERE usando la stringa IV_FILTER_STRING.
+    * @public
+    * @static
+    * @param request express.Request
+    * @return istruzione WHERE.
     */
-   constructor(request: Request) {
-      this._query = new Map<string, string>();
-      this._entity = [];
+   public static whereEntitySet(request: Request): string {
+      let where: string;
 
-      // Memorizza URL:
-      this._originalUrl = request.originalUrl;
+      // Compone l'istruzione WHERE effettuando al sostituzione degli operatori logici:
+      // Condizione di filtro:
+      where = request.body["parameter"]["filter"];
+      where = where.split(" eq ").join(" = ");
+      where = where.split(" ne ").join(" <> ");
+      where = where.split(" gt ").join(" > ");
+      where = where.split(" ge ").join(" >= ");
+      where = where.split(" lt ").join(" < ");
+      where = where.split(" le ").join(" =< ");
 
-      // Scompone l'URl original per determinare la prima entità:
-      this._entity = this.mainEntity();
+      // Risultato:
+      return where === undefined ? "" : where;
+   }
 
-      // Crea una mappa dei parametri presenti nell'URL:
-      for(let key in request.query)
-         this._query.set(key, request.query[key] as string);
+   /**
+    * Compone l'istruzione WHERE usando la tabella IT_KEY_TAB.
+    * @public
+    * @static
+    * @param request express.Request
+    * @return istruzione WHERE.
+    */
+   public static whereEntity(request: Request): string {
+      let where: string;
 
-      // Analizza parametri presenti nell'URL:
-      this._query.forEach((value, key) => {
-         console.log(key, value);
-      });
+      // Compone l'istruzione WHERE effettuando al sostituzione degli operatori logici:
+      // Condizione di filtro:
+      try {
+         where = `id = ${request.body["parameter"]["key"][0]["value"]}`;
+      }
+      catch(e) {
+      }
 
-      console.log(this._entity);
+      // Risultato:
+      return where === undefined ? "" : where;
+   }
+
+   /**
+    * Sostituisce i comandi speciali SAP OData con istruzioni compatibili.
+    * @public
+    * @static
+    * @return istruzione di WHERE compatibile.
+    * @param where
+    */
+   public static specialStatement(where: string): string {
+      let operandPart: string[];
+      let original: string;
+      let operand: string;
+      let value: string;
+      let field: string;
+      let result: string;
+      let start: number;
+      let end: number;
+      let stop: boolean = false;
+
+      // Copia l'istruzione WHERE:
+      result = where;
+
+      // Sostituisce tutti i comandi speciali:
+      while(true) {
+
+         // Verifica quale comando speciale è presente:
+         if(result.includes("substringof")) {
+
+            // Determina la posizione di partenza del comando speciale:
+            start = new RegExp("substringof").exec(result).index;
+
+            // Cerca la prima parentesi chiusa a partire dall'inizio del comando:
+            end = result.indexOf(")", start);
+
+            // L'intera istruzione del comando speciale viene mantenuta così da poter essere usata
+            // per la sostituzione finale (l'ultima parentesi del comando viene mantenuta così da mantenere
+            // la corrispondenza con la parentesi di apertura del comando):
+            original = result.substring(start, end - 1);
+
+            // Estrae gli operatori del comando:
+            operand = result.substring(start + 14, end - 1);
+
+            // Scompone il valore ed il nome del campo:
+            operandPart = operand.split(",");
+
+            // Valorizza valore e nome del campo (elimina anche gli spazi superflui):
+            value = operandPart[0].trim();
+            field = operandPart[1].trim();
+
+            // Prepara l'operatore LIKE aggiungendo il carattere %:
+            if(value.includes("'"))
+               value = `'%${value.substring(1, value.length - 1)}%'`;
+
+            // Sostituisce la parte originale con quella appena creata:
+            result = result.replace(original, field + " LIKE " + value);
+         }
+         else
+            break;
+      }
+
+      // Risultato:
+      return result;
+   }
+
+   /**
+    * Scompone i dati IT_ORDER così da generare l'istruzione ORDER BY.
+    * @public
+    * @static
+    * @param request express.Request
+    * @return istruzione ORDER BY.
+    */
+   public static orderBy(request: Request): string {
+      let property: Object[];
+      let result: string;
+
+      // Copia la lista degli elementi di ordinamento in un array:
+      property = request.body["parameter"]["order"];
+
+      // Concatena le single proprietà nella stringa finale:
+      for(let p of property) {
+         if(!result.length)
+            result = "ORDER BY ";
+         result += `${p["property"]} ${p["order"]}`;
+      }
+
+      // Risultato:
+      return result === undefined ? "" : result;
    }
 }
--- routers/routeRestv2.ts
+++ routers/routeRestv2.ts
@@ -4,12 +4,8 @@
  */
 import express, {Request, Response, Router} from "express";
 import fs from "node:fs";
-import CCustomer, {ICustomer} from "../core/CCustomer";
-import CWbs, {IWbs} from "../core/CWbs";
-import CActivity, {IActivity} from "../core/CActivity";
-import CWorkday, {IWorkday} from "../core/CWorkday";
-import CRequest, {IRequest} from "../core/CRequest";
 import COData from "../core/COData";
+import CCustomer from "../core/CCustomer";
 
 /**
  * Dichiarazioni locali.
@@ -23,7 +19,51 @@ router.get("/$metadata", (request: Request, response: Response) => {
    response.send(fs.readFileSync("./public/metadata_v2.xml").toString());
 });
 
-router.post("/customer", (request: Request, response: Response) => {
-   console.log(request.body);
+/**
+ * Estrae i customer in accordo ai parametri passati dal chiamate OData.
+ */
+router.get("/customer", (request: Request, response: Response) => {
+   let o: CCustomer;
+   let data: Object;
+   let limit: string;
+   let where: string;
+   let orderBy: string;
+   let sqlStatement: string;
+
+   // Prepara istruzione LIMIT insieme ad OFFSET:
+   limit = COData.limit(request);
+
+   // Prepara istruzione di WHERE (1) (*_GET_ENTITYSET):
+   where = COData.whereEntitySet(request);
+
+   // Prepara istruzione di WHERE (2) (*_GET_ENTITY):
+   if(!where.length)
+      where = COData.whereEntity(request);
+
+   // Il filtro contiene comandi speciali:
+   where = COData.specialStatement(where);
+
+   // Condizione di filtro di default:
+   if(!where.length)
+      where = "1 = 1";
+
+   // Prepara ordinamento:
+   orderBy = COData.orderBy(request);
+
+   // Completa l'istruzione SQL:
+   sqlStatement = `SELECT *
+                   FROM main.customer
+                   WHERE ${where} ${limit} ${orderBy}`;
+
+   // Esegue l'istruzione SQL e restituisce il risultato:
+   o = new CCustomer();
+   data = o.executeAll(sqlStatement);
+   response.status(200).send({
+      return: {
+         type: "S",
+         message: "",
+      },
+      data: data
+   });
 });
 
